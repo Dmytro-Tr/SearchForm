b52eeab3557eb1cbab248e45985957ae4e4addb1
Dmtro_Tr <Di4gamegem@gmail.com>
2024-12-25 20:57:00 +0200

    Player

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import axios from "axios";
 import SearchForm from "./components/SearchForm/SearchForm";
 import ArticleList from "./components/ArticleList/ArticleList";
 import fetchArticlesWithTopic from "./api/articles-api";
+import Player from "./components/Player/Player";
 
 const App = () => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-
-  // useEffect(() => {
-  //   async function fetchArticles() {
-  //     try {
-  //       setLoading(true);
-  //       const data = await fetchArticlesWithTopic("react");
-  //       setArticles(data);
-  //     } catch (error) {
-  //       setError(true);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   }
-
-  //   fetchArticles();
-  // }, []);
 
   const handleSearch = async (topic) => {
     try {
@@ -41,8 +26,8 @@ const App = () => {
 
   return (
     <div>
+      <Player source="http://media.w3.org/2010/05/sintel/trailer.mp4" />
       <h1>Latest articles</h1>
-      {/* {articles.length > 0 && <ArticleList items={articles} />} */}
 
       <SearchForm onSearch={handleSearch} />
       {loading && <p style={{ fontSize: 20 }}>Loading data, please wait...</p>}
